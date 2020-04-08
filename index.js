0ab673d5fe1d0c58106f7b0416c8755699a81856
blakelower <blakelower1999@gmail.com>
2020-04-07 17:54:21 -0700

    added POSTBYID

--- index.js
+++ index.js
@@ -2,25 +2,55 @@ const express = require("express");
 
 const Posts = require("./data/seeds/01-posts");
 
+const Comments = require("./data/seeds/02-comments");
+
 const server = express();
 
-server.use(express.json())
-
+server.use(express.json());
 
 //POST
-server.post('/api/posts', (req, res) => {
-    Posts.add(req.body)
-    .then(post => {
+server.post("/api/posts", (req, res) => {
+  Posts.add(req.body)
+    .then((post) => {
+      if (post) {
         res.status(201).json(post);
+      } else {
+        res
+          .status(400)
+          .json({ message: "Please provide title and contents for the post" });
+      }
     })
-    .catch(error => {
-        console.log(error);
-        res.status(400).json({
-            message: "Please provide title and contents for the post"
-        })
+    .catch((error) => {
+      console.log(error);
+      res.status(500).json({
+        message: "There was an error while saving the post to the database",
+      });
+    });
+});
+
+//POST BY ID
+server.post("/api/posts/:id/comments", (req, res) => {
+  Comments.findById(req.params.id)
+    .then((comment) => {
+      if (comment) {
+        res.status(201).json(comment);
+      } else {
+        res
+          .status(400)
+          .json({ message: "Please provide text for the comment." });
+      }
     })
-})
+    .catch((error) => {
+      console.log(error);
+      res
+        .status(500)
+        .json({
+          message:
+            "There was an error while saving the comment to the database",
+        });
+    });
+});
 
 server.listen(4000, () => {
-    console.log("Server Running")
-})
+  console.log("Server Running");
+});
